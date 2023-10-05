e9e8fadeb867de996d75dde10b65214062b2aae7
areejbadar <143810061+areejbadar@users.noreply.github.com>
2023-10-05 22:13:39 +0500

    Add files via upload
    JS-1

---
+++ app.js
@@ -0,0 +1,22 @@
+// Practice 1.1
+// 1
+console.log(4+10)
+// 2
+console.log("Areej Badar")
+
+// Exercise 1.2 - 3
+console.log("Hello World")
+
+// exercise 1.4
+ let a=10 ;  //set a value of 10
+console.log(a) //this will log the value of the variable a to the console
+
+/*This multi-line comment serves as a brief description of what the JavaScript file is about and how it can be used. It's a common practice to include such comments for documentation purposes in your code.*/
+
+// Important Notes
+alert("This is an alert") //alert
+prompt('This is prompt')
+ 
+console.log(Math.random()); //Random Number
+console.log(Math.random() * 100); /*If we want a number between 0 and
+100, we can multiply it by 100, like this:*/
